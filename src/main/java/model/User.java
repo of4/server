d9eeb5c295aebfe5776ea245e2eae4d4af321749
et4 <profour@mail.ru>
2017-02-08 23:04:01 +0400

    4

---
+++ src/main/java/model/User.java
@@ -0,0 +1,51 @@
+package model;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String avatarUrl;
+
+    public User() {
+
+    }
+
+    public User(String id, String name, String email, String avatarUrl) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+}
---
+++ src/main/java/servlets/JsonServlet.java
@@ -0,0 +1,36 @@
+package servlets;
+
+import com.google.gson.Gson;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "JsonServlet", urlPatterns = {"/json"})
+public class JsonServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        User user = new User("322", "xyi", "email", "avatarUrl");
+        String json = new Gson().toJson(user);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+        response.getWriter().flush();
+    }
+}
