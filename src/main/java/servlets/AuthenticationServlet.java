d563bdb55f17a686386b792d65c3bffab74b0a71
et4 <profour@mail.ru>
2017-02-09 23:30:08 +0400

    7

--- src/main/java/servlets/AuthenticationServlet.java
+++ src/main/java/servlets/AuthenticationServlet.java
@@ -7,6 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static util.Constants.JDBC_DATABASE_URL;
 
 @WebServlet(name = "AuthenticationServlet", urlPatterns = {"/authentication"})
 public class AuthenticationServlet extends HttpServlet {
@@ -23,10 +30,25 @@ public class AuthenticationServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        PrintWriter out = response.getWriter();
-        out.print("hello!");
-        out.flush();
+        try {
+            Connection connection = DriverManager.getConnection(JDBC_DATABASE_URL);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+            StringBuilder stringBuilder = new StringBuilder();
+            while (resultSet.next()) {
+                stringBuilder.append(resultSet.getString("name"));
+                stringBuilder.append(resultSet.getString("email"));
+                stringBuilder.append(resultSet.getString("password"));
+                stringBuilder.append("\n");
+            }
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+            PrintWriter out = response.getWriter();
+            out.print(stringBuilder);
+            out.flush();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,6 @@
+package util;
+
+public final class Constants {
+    public static final String JDBC_DATABASE_URL =
+            "postgres://gykxawvnzirykw:987c24570db9e3312c7ddeaaaa6110562dc82d32762aa4d90110f5ba3cba9b67@ec2-54-247-81-76.eu-west-1.compute.amazonaws.com:5432/dnfh1a8qdd5gn";
+}
